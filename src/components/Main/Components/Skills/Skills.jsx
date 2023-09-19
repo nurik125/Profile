88c911a12f807575b87f8eaf618972a541fcbeff
Nurasik12 <nurasylserikov4@gmail.com>
2023-09-19 16:05:51 +0600

    modifing phone version

--- src/components/Main/Components/Skills/Skills.jsx
+++ src/components/Main/Components/Skills/Skills.jsx
@@ -4,7 +4,19 @@ import "./Skills.css";
 const Skills = () => {
   return (
     <div className="Skills">
-      <div id="Skills">My experience: </div>
+      <div id="Skills">My honest reaction: </div>
+      <div>
+        <img
+          className="gun-image"
+          src={require("../../../../FirstShot.jpg")}
+          alt="gun"
+        />
+        <img
+          className="human"
+          src="https://th.bing.com/th/id/OIP.Q7iL_BN8uiZFc-U18yWhkQHaHa?pid=ImgDet&rs=1"
+          alt="human"
+        />
+      </div>
     </div>
   );
 };
